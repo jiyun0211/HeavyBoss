c4cc22cea5977771c75db1e40b13e8e92e3691f8
jiyun0211 <jiyun5474@gmail.com>
2025-07-28 17:25:51 +0900

    create WBPs and stamina bar

---
+++ Source/MAV_BossFight/Public/WarriorGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "WarriorGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAV_BOSSFIGHT_API UWarriorGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
