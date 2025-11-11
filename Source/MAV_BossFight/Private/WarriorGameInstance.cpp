d49072a8d16d04526ef8580652404bf0b8286fb2
jiyun0211 <jiyun5474@gmail.com>
2025-11-11 15:58:57 +0900

    PlayFab edited

--- Source/MAV_BossFight/Private/WarriorGameInstance.cpp
+++ Source/MAV_BossFight/Private/WarriorGameInstance.cpp
@@ -1,5 +1,75 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "WarriorGameInstance.h"
+#include "PlayFabClientAPI.h"
+#include "PlayFabClientModels.h"
+#include "Misc/Guid.h"
+#include "Kismet/GameplayStatics.h"
 
+using namespace PlayFab;
+using namespace ClientModels;
+
+void UWarriorGameInstance::Init()
+{
+	Super::Init();
+
+	UE_LOG(LogTemp, Log, TEXT("WarriorGameInstance Init 실행됨. 로그인 시도..."));
+	//LoginWithDeviceID();
+}
+
+void UWarriorGameInstance::LoginWithDeviceID()
+{
+    FLoginWithCustomIDRequest Request;
+
+    FString DeviceId = FPlatformMisc::GetDeviceId();
+    if (DeviceId.IsEmpty())
+    {
+        DeviceId = FGuid::NewGuid().ToString(EGuidFormats::Digits);
+    }
+
+    Request.CustomId = DeviceId;
+    Request.CreateAccount = true;
+
+    /*UPlayFabClientAPI::LoginWithCustomID(
+        Request,
+        PlayFab::UPlayFabClientAPI::FLoginWithCustomIDDelegate::CreateUObject(this, &UWarriorGameInstance::OnLoginSuccess),
+        PlayFab::FPlayFabErrorDelegate::CreateUObject(this, &UWarriorGameInstance::OnLoginError)
+    );*/
+}
+
+void UWarriorGameInstance::OnLoginSuccess(const FLoginResult& Result)
+{
+    UE_LOG(LogTemp, Log, TEXT("로그인 성공! PlayFabId: %s"), *Result.PlayFabId);
+}
+
+void UWarriorGameInstance::OnLoginError(const FPlayFabCppError& Error)
+{
+    UE_LOG(LogTemp, Error, TEXT("로그인 실패! Error: %s"), *Error.ErrorMessage);
+}
+
+void UWarriorGameInstance::SetStartTime()
+{
+    StartTime = UGameplayStatics::GetTimeSeconds(GetWorld());
+}
+
+void UWarriorGameInstance::SetEndTime()
+{
+    EndTime = UGameplayStatics::GetTimeSeconds(GetWorld());
+}
+
+void UWarriorGameInstance::RecordClearTime()
+{
+    ClearTime = EndTime - StartTime;
+}
+
+void UWarriorGameInstance::SaveClearTime(float Time)
+{
+    FString TimeString = FString::SanitizeFloat(Time);
+
+    PlayFab::ClientModels::FUpdateUserDataRequest Request;
+    Request.Data.Add("ClearTime", TimeString);
+
+    UPlayFabClientAPI::UpdateUserData(Request,
+        PlayFab::UPlayFabClientAPI::FUpdateUserDataDelegate::CreateUObject(this, &UWarriorGameInstance::OnSaveSuccess),
+        PlayFab::FPlayFabErrorDelegate::CreateUObject(this, &UWarriorGameInstance::OnSaveError)
+    );
+}
--- Source/MAV_BossFight/Public/WarriorGameInstance.h
+++ Source/MAV_BossFight/Public/WarriorGameInstance.h
@@ -4,6 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
+#include "PlayFab.h"
+#include "Core/PlayFabError.h"
+#include "Core/PlayFabClientDataModels.h"
+#include "PlayFabClientDataModels.h"
+#include "PlayFabClientAPI.h"
 #include "WarriorGameInstance.generated.h"
 
 /**
@@ -14,4 +19,33 @@ class MAV_BOSSFIGHT_API UWarriorGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
 	
+public:
+	virtual void Init() override;
+
+	UPROPERTY(BlueprintReadWrite);
+	float StartTime = 0.0f;
+
+	UPROPERTY(BlueprintReadWrite);
+	float EndTime = 0.0f;
+
+	UPROPERTY(BlueprintReadWrite);
+	float ClearTime = 0.0f;
+
+	UFUNCTION(BlueprintCallable)
+	void SetStartTime();
+
+	UFUNCTION(BlueprintCallable)
+	void SetEndTime();
+
+	UFUNCTION(BlueprintCallable)
+	void RecordClearTime();
+
+	UFUNCTION(BlueprintCallable)
+	void SaveClearTime(float Time);
+
+private:
+	void LoginWithDeviceID();
+
+	void OnLoginSuccess(const PlayFab::ClientModels::FLoginResult& Result);
+	void OnLoginError(const PlayFab::FPlayFabCppError& Error);
 };
